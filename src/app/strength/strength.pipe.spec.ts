159f34a65e4bb38ad0e6e6482e96de4b9830228c
mmokgethi <modisa@ModisaM.localdomain>
2019-10-14 16:17:45 +0200

    pipe testing

--- src/app/strength/strength.pipe.spec.ts
+++ src/app/strength/strength.pipe.spec.ts
@@ -2,20 +2,20 @@ import { StrengthPipe } from './strength.pipe';
 
 describe('StrengthPipe', () => {
   it('should display weak if strenth is 5', () => {
-    let pipe = new StrengthPipe();
+    const pipe = new StrengthPipe();
 
     expect(pipe.transform(5)).toEqual('5(weak)');
   });
 
   it('should display strong if strength is 10', () => {
-    let pipe = new StrengthPipe();
+    const pipe = new StrengthPipe();
 
     expect(pipe.transform(10)).toEqual('10(strong)');
   });
 
-  it('should display unbelivable if strength is > 20', () => {
-    let pipe = new StrengthPipe();
+  it('should display unbelivable if strength is >= 20', () => {
+    const pipe = new StrengthPipe();
 
-    expect(pipe.transform(21)).toEqual('21(unbelievable)');
+    expect(pipe.transform(21)).toEqual('1(unbelievable)');
   });
 });
